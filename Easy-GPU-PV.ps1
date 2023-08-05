8aa8f35554ddb6260e523812aede599006155059
Car_Killer <przemekwolw@gmail.com>
2023-08-05 23:49:25 +0200

    Refactored code
    Rewritten most of the script

---
+++ Easy-GPU-PV.ps1
@@ -0,0 +1,344 @@
+#Easier script for adding, updating, removing GPUs from Hyper-V machines
+#Make sure to enabled ehanced session to enable audio support...
+
+[int]$choice = Read-Host -Prompt 'What do you want to do:
+1. Add GPU to Existing Virtual Machine
+2. Update GPU Drivers in Virtual Machine
+3. Remove GPU from Virtual Machine '
+
+if ($choice -eq 1) {
+    "Adding GPU to VM"
+    Import-Module $PSSCriptRoot\Add-GPUPartitiontoExistingVM.psm1
+
+    "Checking for available GPUs"
+    Function Get-DesktopPC
+    {
+     $isDesktop = $true
+     if(Get-WmiObject -Class win32_systemenclosure | Where-Object { $_.chassistypes -eq 9 -or $_.chassistypes -eq 10 -or $_.chassistypes -eq 14})
+       {
+       Write-Warning "Thunderbolt 3 or 4 dock based GPU's may work"
+       $isDesktop = $false }
+     if (Get-WmiObject -Class win32_battery)
+       { $isDesktop = $false }
+     $isDesktop
+    }
+
+    Function Get-WindowsCompatibleOS {
+    $build = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+    if ($build.CurrentBuild -ge 19041 -and ($($build.editionid -like 'Professional*') -or $($build.editionid -like 'Enterprise*') -or $($build.editionid -like 'Education*'))) {
+        Return $true
+        }
+    Else {
+        Write-Warning "Only Windows 10 20H1 or Windows 11 (Pro or Enterprise) is supported"
+        Return $false
+        }
+    }
+
+
+    Function Get-HyperVEnabled {
+    if (Get-WindowsOptionalFeature -Online | Where-Object FeatureName -Like 'Microsoft-Hyper-V-All'){
+        Return $true
+        }
+    Else {
+        Write-Warning "You need to enable Virtualisation in your motherboard and then add the Hyper-V Windows Feature and reboot"
+        Return $false
+        }
+    }
+
+    Function Get-WSLEnabled {
+        if ((wsl -l -v)[2].length -gt 1 ) {
+            Write-Warning "WSL is Enabled. This may interferre with GPU-P and produce an error 43 in the VM"
+            Return $true
+            }
+        Else {
+            Return $false
+            }
+    }
+
+    Function Get-VMGpuPartitionAdapterFriendlyName {
+        $Devices = (Get-WmiObject -Class "Msvm_PartitionableGpu" -ComputerName $env:COMPUTERNAME -Namespace "ROOT\virtualization\v2").name
+        Foreach ($GPU in $Devices) {
+            $GPUParse = $GPU.Split('#')[1]
+            Get-WmiObject Win32_PNPSignedDriver | where {($_.HardwareID -eq "PCI\$GPUParse")} | select DeviceName -ExpandProperty DeviceName
+            }
+    }
+
+    If ((Get-DesktopPC) -and  (Get-WindowsCompatibleOS) -and (Get-HyperVEnabled)) {
+        "System Compatible"
+        $build = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+        if ($build.CurrentBuild -ge 22000) {
+            $gpuTable = @{}
+            $gpuOut = Get-VMGpuPartitionAdapterFriendlyName
+            $count = 0
+            ForEach ($line in $($gpuOut -split "`r`n"))
+            {
+                $count = $count + 1
+                $gpuTable.Add($count, $Line)
+            }
+            "Select GPU you want to attach to VM"
+            $count = 0
+            ForEach ($key in $gpuTable.keys)
+            {
+                $message = '{0}. {1}' -f $key, $gpuTable[$key]
+                Write-Output $message
+            }
+            [int]$selection = Read-Host -Prompt 'GPU: '
+            if ($gpuTable.ContainsKey($selection)) {
+                [string]$selectedGpu = '{1}' -f $selection, $gpuTable[$selection]
+                "Selected GPU: $selectedGpu"
+
+                "Checking for available virtual machines"
+                $vmTable = @{}
+                $GetVM = Get-VM
+                $count = 0
+                ForEach ($line in $($GetVM -split "`r`n"))
+                {
+                    $count = $count + 1
+                    $line = $line.replace("VirtualMachine (Name = '","").replace("') ","").replace("[","") -replace ('Id = .*', '')
+                    $vmTable.Add($count, $Line)
+                }
+                "Select Virtual Machine"
+                $count = 0
+                ForEach ($key in $vmTable.keys)
+                {
+                    $message = '{0}. {1}' -f $key, $vmTable[$key]
+                    Write-Output $message
+                }
+                [int]$selection = Read-Host -Prompt 'Virtual Machine: '
+                if ($vmTable.ContainsKey($selection)) {
+                    [string]$selectedVM = '{1}' -f $selection, $vmTable[$selection]
+                    "Selected VM: $selectedVM"
+                    [int]$perc = Read-Host -Prompt 'Enter GPU Allocation Percentage: '
+                    Add-GPUPartitiontoExistingVM -VMName $selectedVM -GPUName $selectedGpu -Hostname $ENV:Computername -GPUResourceAllocationPercentage $perc
+                }
+                else {
+                    'This VM does not exists'
+                    Read-Host -Prompt "Press Enter to Exit"
+                }
+              }
+            else {
+                'This GPU does not exists'
+                Read-Host -Prompt "Press Enter to Exit"
+            }
+        }
+        else {
+            $selectedGpu = 'AUTO'
+            "Windows 10 Detected"
+            "Selected GPU: $selectedGpu"
+
+            "Checking for available virtual machines"
+            $vmTable = @{}
+            $GetVM = Get-VM
+            $count = 0
+            ForEach ($line in $($GetVM -split "`r`n"))
+            {
+                $count = $count + 1
+                $line = $line.replace("VirtualMachine (Name = '","").replace("') ","").replace("[","") -replace ('Id = .*', '')
+                $vmTable.Add($count, $Line)
+            }
+            "Select Virtual Machine"
+            $count = 0
+            ForEach ($key in $vmTable.keys)
+            {
+                $message = '{0}. {1}' -f $key, $vmTable[$key]
+                Write-Output $message
+            }
+            [int]$selection = Read-Host -Prompt 'Virtual Machine: '
+            if ($vmTable.ContainsKey($selection)) {
+                [string]$selectedVM = '{1}' -f $selection, $vmTable[$selection]
+                "Selected VM: $selectedVM"
+                [int]$perc = Read-Host -Prompt 'Enter GPU Allocation Percentage: '
+                Add-GPUPartitiontoExistingVM -VMName $selectedVM -GPUName $selectedGpu -Hostname $ENV:Computername -GPUResourceAllocationPercentage $perc
+            }
+            else {
+                'This VM does not exists'
+                Read-Host -Prompt "Press Enter to Exit"
+            }
+        }
+    }
+    else {
+    Read-Host -Prompt "Press Enter to Exit"
+    }
+}
+if ($choice -eq 2) {
+    "Updating Drivers in VM"
+    Import-Module $PSSCriptRoot\Update-VMGpuPartitionDriver.psm1
+
+    "Checking for available GPUs"
+    Function Get-DesktopPC
+    {
+     $isDesktop = $true
+     if(Get-WmiObject -Class win32_systemenclosure | Where-Object { $_.chassistypes -eq 9 -or $_.chassistypes -eq 10 -or $_.chassistypes -eq 14})
+       {
+       Write-Warning "Thunderbolt 3 or 4 dock based GPU's may work"
+       $isDesktop = $false }
+     if (Get-WmiObject -Class win32_battery)
+       { $isDesktop = $false }
+     $isDesktop
+    }
+
+    Function Get-WindowsCompatibleOS {
+    $build = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+    if ($build.CurrentBuild -ge 19041 -and ($($build.editionid -like 'Professional*') -or $($build.editionid -like 'Enterprise*') -or $($build.editionid -like 'Education*'))) {
+        Return $true
+        }
+    Else {
+        Write-Warning "Only Windows 10 20H1 or Windows 11 (Pro or Enterprise) is supported"
+        Return $false
+        }
+    }
+
+
+    Function Get-HyperVEnabled {
+    if (Get-WindowsOptionalFeature -Online | Where-Object FeatureName -Like 'Microsoft-Hyper-V-All'){
+        Return $true
+        }
+    Else {
+        Write-Warning "You need to enable Virtualisation in your motherboard and then add the Hyper-V Windows Feature and reboot"
+        Return $false
+        }
+    }
+
+    Function Get-WSLEnabled {
+        if ((wsl -l -v)[2].length -gt 1 ) {
+            Write-Warning "WSL is Enabled. This may interferre with GPU-P and produce an error 43 in the VM"
+            Return $true
+            }
+        Else {
+            Return $false
+            }
+    }
+
+    Function Get-VMGpuPartitionAdapterFriendlyName {
+        $Devices = (Get-WmiObject -Class "Msvm_PartitionableGpu" -ComputerName $env:COMPUTERNAME -Namespace "ROOT\virtualization\v2").name
+        Foreach ($GPU in $Devices) {
+            $GPUParse = $GPU.Split('#')[1]
+            Get-WmiObject Win32_PNPSignedDriver | where {($_.HardwareID -eq "PCI\$GPUParse")} | select DeviceName -ExpandProperty DeviceName
+            }
+    }
+
+    If ((Get-DesktopPC) -and  (Get-WindowsCompatibleOS) -and (Get-HyperVEnabled)) {
+        "System Compatible"
+        $build = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+            if ($build.CurrentBuild -ge 22000) {
+            $gpuTable = @{}
+            $gpuOut = Get-VMGpuPartitionAdapterFriendlyName
+            $count = 0
+            ForEach ($line in $($gpuOut -split "`r`n"))
+            {
+                $count = $count + 1
+                $gpuTable.Add($count, $Line)
+            }
+            "Select GPU you want to attach to VM"
+            $count = 0
+            ForEach ($key in $gpuTable.keys)
+            {
+                $message = '{0}. {1}' -f $key, $gpuTable[$key]
+                Write-Output $message
+            }
+            [int]$selection = Read-Host -Prompt 'GPU: '
+            if ($gpuTable.ContainsKey($selection)) {
+                [string]$selectedGpu = '{1}' -f $selection, $gpuTable[$selection]
+                "Selected GPU: $selectedGpu"
+
+                "Checking for available virtual machines"
+                $vmTable = @{}
+                $GetVM = Get-VM
+                $count = 0
+                ForEach ($line in $($GetVM -split "`r`n"))
+                {
+                    $count = $count + 1
+                    $line = $line.replace("VirtualMachine (Name = '","").replace("') ","").replace("[","") -replace ('Id = .*', '')
+                    $vmTable.Add($count, $Line)
+                }
+                "Select Virtual Machine"
+                $count = 0
+                ForEach ($key in $vmTable.keys)
+                {
+                    $message = '{0}. {1}' -f $key, $vmTable[$key]
+                    Write-Output $message
+                }
+                [int]$selection = Read-Host -Prompt 'Virtual Machine: '
+                if ($vmTable.ContainsKey($selection)) {
+                    [string]$selectedVM = '{1}' -f $selection, $vmTable[$selection]
+                    "Selected VM: $selectedVM"
+                    Update-VMGpuPartitionDriver -VMName $selectedVM -GPUName $selectedGpu -Hostname $ENV:Computername
+                }
+                else {
+                    'This VM does not exists'
+                    Read-Host -Prompt "Press Enter to Exit"
+                }
+              }
+            else {
+                'This GPU does not exists'
+                Read-Host -Prompt "Press Enter to Exit"
+            }
+        }
+        else {
+                $selectedGpu = 'AUTO'
+                "Windows 10 Detected"
+                "Selected GPU: $selectedGpu"
+                "Checking for available virtual machines"
+                $vmTable = @{}
+                $GetVM = Get-VM
+                $count = 0
+                ForEach ($line in $($GetVM -split "`r`n"))
+                {
+                    $count = $count + 1
+                    $line = $line.replace("VirtualMachine (Name = '","").replace("') ","").replace("[","") -replace ('Id = .*', '')
+                    $vmTable.Add($count, $Line)
+                }
+                "Select Virtual Machine"
+                $count = 0
+                ForEach ($key in $vmTable.keys)
+                {
+                    $message = '{0}. {1}' -f $key, $vmTable[$key]
+                    Write-Output $message
+                }
+                [int]$selection = Read-Host -Prompt 'Virtual Machine: '
+                if ($vmTable.ContainsKey($selection)) {
+                    [string]$selectedVM = '{1}' -f $selection, $vmTable[$selection]
+                    "Selected VM: $selectedVM"
+                    Update-VMGpuPartitionDriver -VMName $selectedVM -GPUName $selectedGpu -Hostname $ENV:Computername
+                }
+                else {
+                    'This VM does not exists'
+                    Read-Host -Prompt "Press Enter to Exit"
+                }
+        }
+    }
+    else {
+    Read-Host -Prompt "Press Enter to Exit"
+    }
+}
+if ($choice -eq 3) {
+    "Removing GPUs from VM"
+    "Checking for available virtual machines"
+    $vmTable = @{}
+    $GetVM = Get-VM
+    $count = 0
+    ForEach ($line in $($GetVM -split "`r`n"))
+    {
+        $count = $count + 1
+        $line = $line.replace("VirtualMachine (Name = '","").replace("') ","").replace("[","") -replace ('Id = .*', '')
+        $vmTable.Add($count, $Line)
+    }
+    "Select Virtual Machine"
+    $count = 0
+    ForEach ($key in $vmTable.keys)
+    {
+        $message = '{0}. {1}' -f $key, $vmTable[$key]
+        Write-Output $message
+    }
+    [int]$selection = Read-Host -Prompt 'Virtual Machine: '
+    if ($vmTable.ContainsKey($selection)) {
+        [string]$selectedVM = '{1}' -f $selection, $vmTable[$selection]
+        "Selected VM: $selectedVM"
+        Remove-VMGpuPartitionAdapter -VMName $selectedVM
+        "GPU Removed"
+    }
+    else {
+        'This VM does not exists'
+        Read-Host -Prompt "Press Enter to Exit"
+    }
+}
--- README.md
+++ README.md
@@ -6,68 +6,24 @@ GPU-PV allows you to partition your systems dedicated or integrated GPU and assi
 
 Easy-GPU-PV aims to make this easier by automating the steps required to get a GPU-PV VM up and running.
 Easy-GPU-PV does the following...
-1) Creates a VM of your choosing
-2) Automatically Installs Windows to the VM
-3) Partitions your GPU of choice and copies the required driver files to the VM
+1. Allows managing GPUs in your VM
+2. Adds GPU to existing VM
+3. Updates Drivers in VM
+4. Removes GPUs from VM
 
 ### Prerequisites:
 * Windows 10 20H1+ Pro, Enterprise or Education OR Windows 11 Pro, Enterprise or Education.  Windows 11 on host and VM is preferred due to better compatibility.
-* Matched Windows versions between the host and VM. Mismatches may cause compatibility issues, blue-screens, or other issues. (Win10 21H1 + Win10 21H1, or Win11 21H2 + Win11 21H2, for example)
 * Desktop Computer with dedicated NVIDIA/AMD GPU or Integrated Intel GPU - Laptops with NVIDIA GPUs are not supported at this time, but Intel integrated GPUs work on laptops.  GPU must support hardware video encoding (NVIDIA NVENC, Intel Quicksync or AMD AMF).
 * Latest GPU driver from Intel.com or NVIDIA.com, don't rely on Device manager or Windows update.
-* Latest Windows 10 ISO [downloaded from here](https://www.microsoft.com/en-gb/software-download/windows10ISO) / Windows 11 ISO [downloaded from here.](https://www.microsoft.com/en-us/software-download/windows11) - Do not use Media Creation Tool, if no direct ISO link is available, follow [this guide.](https://www.nextofwindows.com/downloading-windows-10-iso-images-using-rufus)
 * Virtualisation enabled in the motherboard and [Hyper-V fully enabled](https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/enable-hyper-v) on the Windows 10/ 11 OS (requires reboot).
 * Allow Powershell scripts to run on your system - typically by running "Set-ExecutionPolicy unrestricted" in Powershell running as Administrator.
 
 ### Instructions
 1. Make sure your system meets the prerequisites.
 2. Download files from this repository
-3. Search your system for Powershell ISE and run as Administrator.
-4. In the extracted folder you downloaded, open ```PreChecks.ps1``` in Powershell ISE.  Run the files from within the extracted folder. Do not move them.
-5. Open and Run ```PreChecks.ps1``` in Powershell ISE using the green play button and copy the GPU Listed (or the warnings that you need to fix).
-6. Open ```CopyFilesToVM.ps1``` Powershell ISE and edit the params section at the top of the file, you need to be careful about how much ram, storage and hard drive you give it as your system needs to have that available.  On Windows 10 the GPUName must be left as "AUTO", In Windows 11 it can be either "AUTO" or the specific name of the GPU you want to partition exactly how it appears in PreChecks.ps1.  Additionally, you need to provide the path to the Windows 10/11 ISO file you downloaded.
-7. Run ```CopyFilesToVM.ps1``` with your changes to the params section - this may take 5-10 minutes.
-8. You should be good to go!
-
-### Adding GPU to existing VM
-1. Make sure your system meets the prerequisites.
-2. Download files from this repository
-3. Search your system for Powershell ISE and run as Administrator.
-4. In the extracted folder you downloaded, open ```PreChecks.ps1``` in Powershell ISE.  Run the files from within the extracted folder. Do not move them.
-5. Open and ```Run PreChecks.ps1``` in Powershell ISE using the green play button and copy the GPU Listed (or the warnings that you need to fix).
-6. Open ```Add-GPUPartitiontoExistingVM.ps1``` Powershell ISE and edit the params section at the top of the file. On Windows 10 the GPUName must be left as "AUTO", In Windows 11 it can be either "AUTO" or the specific name of the GPU you want to partition exactly how it appears in PreChecks.ps1.
-7. Run ```Add-GPUPartitiontoExistingVM.ps1``` with your changes to the params section - this may take around a minute.
-8. You should be good to go!
-
-### Removing GPU from VM
-1. Open PowerShell as Admin
-2. Run this command ```Remove-VMGpuPartitionAdapter -VMName "YOUR VM NAME"```
-3. Your VM won't use GPU anymore, or you can add different GPU.
-
-### Upgrading GPU Drivers when you update the host GPU Drivers
-It's important to update the VM GPU Drivers after you have updated the Host GPUs drivers. You can do this by...
-1. Reboot the host after updating GPU Drivers.
-2. Open Powershell as administrator and change directory (CD) to the path that CopyFilesToVM.ps1 and Update-VMGpuPartitionDriver.ps1 are located.
-3. Run ```Update-VMGpuPartitionDriver.ps1 -VMName "Name of your VM" -GPUName "Name of your GPU"```    (Windows 10 GPU name must be "AUTO")
-
-### Values
-  ```VMName = "GPUP"``` - Name of VM in Hyper-V and the computername / hostname
-  ```SourcePath = "C:\Users\james\Downloads\Win11_English_x64.iso"``` - path to Windows 10/ 11 ISO on your host
-  ```Edition    = 6``` - Leave as 6, this means Windows 10/11 Pro
-  ```VhdFormat  = "VHDX"``` - Leave this value alone
-  ```DiskLayout = "UEFI"``` - Leave this value alone
-  ```SizeBytes  = 40gb``` - Disk size, in this case 40GB, the minimum is 20GB
-  ```MemoryAmount = 8GB``` - Memory size, in this case 8GB
-  ```CPUCores = 4``` - CPU Cores you want to give VM, in this case 4
-  ```NetworkSwitch = "Default Switch"``` - Leave this alone unless you're not using the default Hyper-V Switch
-  ```VHDPath = "C:\Users\Public\Documents\Hyper-V\Virtual Hard Disks\"``` - Path to the folder you want the VM Disk to be stored in, it must already exist
-  ```UnattendPath = "$PSScriptRoot"+"\autounattend.xml"``` -Leave this value alone
-  ```GPUName = "AUTO"``` - AUTO selects the first available GPU. On Windows 11 you may also use the exact name of the GPU you want to share with the VM in multi GPU situations (GPU selection is not available in Windows 10 and must be set to AUTO)
-  ```GPUResourceAllocationPercentage = 50``` - Percentage of the GPU you want to share with the VM
-  ```Username = "GPUVM"``` - The VM Windows Username, do not include special characters, and must be different from the "VMName" value you set
-  ```Password = "CoolestPassword!"``` - The VM Windows Password, cannot be blank
-  ```Autologon = "true"```- If you want the VM to automatically login to the Windows Desktop
-
+3. Install Windows on your VM
+4. Run ```RUN-ME.BAT``` as Administrator
+5. Follow the steps in command prompt
 
 ### Thanks to:
 - [Hyper-ConvertImage](https://github.com/tabs-not-spaces/Hyper-ConvertImage) for creating an updated version of [Convert-WindowsImage](https://github.com/MicrosoftDocs/Virtualization-Documentation/tree/master/hyperv-tools/Convert-WindowsImage) that is compatible with Windows 10 and 11.
@@ -75,13 +31,11 @@ It's important to update the VM GPU Drivers after you have updated the Host GPUs
 
 
 ### Notes:
-- If you get "ERROR  : Cannot bind argument to parameter 'Path' because it is null." this probably means you used Media Creation Tool to download the ISO.  You unfortunately cannot use that, if you don't see a direct ISO download link at the Microsoft page, follow [this guide.](https://www.nextofwindows.com/downloading-windows-10-iso-images-using-rufus)
 - Your GPU on the host will have a Microsoft driver in device manager, rather than an nvidia/intel/amd driver. As long as it doesn't have a yellow triangle over top of the device in device manager, it's working correctly.
 - The screen may go black for times up to 10 seconds in situations when UAC prompts appear, applications go in and out of fullscreen and when you switch between video codecs in Parsec - not really sure why this happens, it's unique to GPU-P machines and seems to recover faster at 1280x720.
 - Vulkan renderer is unavailable and GL games may or may not work.  [This](https://www.microsoft.com/en-us/p/opencl-and-opengl-compatibility-pack/9nqpsl29bfff?SilentAuth=1&wa=wsignin1.0#activetab=pivot:overviewtab) may help with some OpenGL apps.
 - If you do not have administrator permissions on the machine it means you set the username and vmname to the same thing, these needs to be different.
 - AMD Polaris GPUS like the RX 580 do not support hardware video encoding via GPU Paravirtualization at this time.
-- To download Windows ISOs with Rufus, it must have "Check for updates" enabled.
 
 ### Original Project:
 - Original Project is available [here.](https://github.com/jamesstringerparsec/Easy-GPU-PV)
